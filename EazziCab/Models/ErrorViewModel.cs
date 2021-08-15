8c7655ce521a30f59df1afd1957061fde4a88cb3
Tolulopedd <tolulope.davids@yahoo.com>
2021-08-15 21:33:43 +0100

    first Build

---
+++ EazziCab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EazziCab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
